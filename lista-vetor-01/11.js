4c456457b2359a719376a4efdcc3ca9dac202eaf
gustavohmello <gustavohmello@gmail.com>
2026-03-25 17:12:53 -0300

    11.js da lista-for-01

---
+++ lista-vetor-01/11.js
@@ -0,0 +1,13 @@
+// inverter um alray em outro (sem reverse)//
+
+let a = [1, 2, 3, 4, 5];
+let b = [];
+let w = a.length - 1;
+for (let i = 0; i < a.length; i++) {
+
+    b[i] = a[w];
+    w--
+    
+}
+
+console.log(b)
