5b0dde867cb7228f137e1b31c7563d0dd4de2e15
gustavohmello <gustavohmello@gmail.com>
2026-03-25 16:29:21 -0300

    7.js da lista-vetor-01

---
+++ lista-vetor-01/7.js
@@ -0,0 +1,14 @@
+let number = [1, 2, 3, 4, 5, 6, 7, 8];
+let pares = 0;
+
+for (let i = 0; i < number.length; i++){
+   
+     if(number[i] % 2 === 0){
+        pares++;
+
+}
+
+}
+
+console.log(pares)
+
