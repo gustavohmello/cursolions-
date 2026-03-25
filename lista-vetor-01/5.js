3040a955e3f8eceaeccf1291f12871f4cee9fb81
gustavohmello <gustavohmello@gmail.com>
2026-03-25 15:15:01 -0300

    5.js da lista-vetor-01

---
+++ lista-vetor-01/5.js
@@ -0,0 +1,11 @@
+let number = [1,4,6,5,9,10];
+let numeroMaior = 0;
+for (let i = 0;i<number.length; i++){
+
+    if(number [i] > numeroMaior){
+        numeroMaior = number[i]
+    }
+
+
+}
+console.log(numeroMaior);
