9dfcb25cda080291525d77652382ba638cacd930
gustavohmello <gustavohmello@gmail.com>
2026-03-26 15:52:29 -0300

    1.js da lista-vetor-02

--- lista-vetor-02/1.js
+++ lista-vetor-02/1.js
@@ -3,14 +3,16 @@
 let numbers = [1, 2, 85, 4, 5,90];
 let numeroMaior = numbers[0];
 let segundoMaior = numbers[0]
-
+let terceiroMaior = numbers[0];
 
 for (i = 0; i < numbers.length; i++) {
 
     if (numbers[i] > numeroMaior) {
+        segundoMaior = numeroMaior;
         numeroMaior = numbers[i];
 
     } else if (segundoMaior < numbers[i]) {
+        terceiroMaior = segundoMaior;
         segundoMaior = numbers[i];
 
     }
