f2aba1f1ffd7a7bdfd198694536e8d6760f55b90
gustavohmello <gustavohmello@gmail.com>
2026-03-26 15:50:38 -0300

    15.js da lista-vetor-01

--- lista-vetor-01/15.js
+++ lista-vetor-01/15.js
@@ -1,1 +1,16 @@
-let number = []
+let number = [1,2,3,4,5,4,7,8,5];
+let crescente = true;
+
+for(i = 0; i<number; i++){
+    if(number[i] > number [i+1]){
+        crescente = false;
+    }
+    
+}
+
+if(crescente){
+    console.log("crescente");
+
+}else{
+    console.log("Não crescente")
+}
